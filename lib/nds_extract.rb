7be822f2d6c4fedfc9b6303132d612de69a1acbd
Cole Rau <colevrau@gmail.com>
2019-11-13 09:55:46 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -49,8 +49,7 @@ def total_gross(source)
   
   pp directors_totals(source)
   total = 0 
-  directors_totals(source).each do |director|
-    total += director.value
+  directors_totals(source).reduce(0) { |sum, (key, valye)
   end 
   total 
 end
